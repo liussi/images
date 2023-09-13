485547930110be7c1fb76b53840acbf7614be43e
liussi <125599487+liussi@users.noreply.github.com>
2023-09-13 15:18:24 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -48,7 +48,6 @@
 import React, { Component } from 'react';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
-import Button from './Button/Button';
 
 export class App extends Component {
   state = {
