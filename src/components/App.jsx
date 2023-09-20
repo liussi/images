3d3d2a5521adf36b46a6d658f1334bc6b9648e9c
liussi <125599487+liussi@users.noreply.github.com>
2023-09-20 22:52:01 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -47,7 +47,6 @@ function App() {
     }
   };
 
- 
   useEffect(() => {
     if (imageName === '') return;
 
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,34 +2,32 @@ import React, { useEffect } from 'react';
 import { Overlay, ModalEl } from './Modal.styled';
 
 export default function Modal({image,onClose}){
-
   useEffect(() => {
-     
-     window.addEventListener('keydown', hendleEsc);
+    window.addEventListener('keydown', hendleEsc);
 
-     return () => {
-       window.removeEventListener('keydown', hendleEsc);
-     };
-   }, []);
-   
-   const hendleEsc = e => {
-     if (e.code === 'Escape') {
-       onClose();
-     }
-   };
+    return () => {
+      window.removeEventListener('keydown', hendleEsc);
+    };
+  }, [hendleEsc]);
 
- const  hendleBackdrop = e => {
+  function hendleEsc(e) {
+    if (e.code === 'Escape') {
+      onClose();
+    }
+  }
+
+  function hendleBackdrop (e) {
     if (e.target === e.currentTarget) {
       onClose();
     }
   };
 
-    return (
-      <Overlay onClick={hendleBackdrop}>
-        <ModalEl>
-          <img src={image} alt="" width="100%" />
-        </ModalEl>
-      </Overlay>
-    );
-  }
+  return (
+    <Overlay onClick={hendleBackdrop}>
+      <ModalEl>
+        <img src={image} alt="" width="100%" />
+      </ModalEl>
+    </Overlay>
+  );
+}
 
